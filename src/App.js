b81b4947a7b555027db18c39aff2a977ab5dcf67
deyonuz <diyorbekabduxoliqov2003@gmail.com>
2023-12-22 04:03:59 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ContextExample from './ContextComponent/ContextExample';
+
+function App() {
+  return (
+    <div className="App">
+      <ContextExample/>
+     
+    </div>
+  );
+}
+
+export default App;
